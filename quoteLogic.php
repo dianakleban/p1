0bab0a7c79a2be00b315dfe1e33448849f057462
Diana Kleban <dik548@g.harvard.edu>
2017-09-11 22:27:10 -0500

    Added a new quote to quoteLogic file

--- quoteLogic.php
+++ quoteLogic.php
@@ -2,7 +2,8 @@
 $phrases = [
     'The secret of getting ahead is getting started!',
     'It is better to keep your mouth closed and let people think you are a fool than to open it and remove all doubt!',
-    'Good friends, good books and a sleepy conscience: this is the ideal life!'
+    'Good friends, good books and a sleepy conscience: this is the ideal life!',
+    'The two most important days in your life are the day you are born and the day you find out why!'
 ];
 
 shuffle($phrases);
